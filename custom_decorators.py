e2bc7b308a36414ef506f6385c0df0089d6a8036
Morteza Salarkia <msalarkia3@gmail.com>
2020-05-17 02:02:59 +0430

    example added

--- custom_decorators.py
+++ custom_decorators.py
@@ -6,7 +6,15 @@ def notify_function_call(fn):
         print(f'{fn.__name__} called!')
         result = fn(*args, **kwargs)
         print(f'{fn.__name__} ended!')
-	return result
+        return result
+
     return decorated_function
 
 
+if __name__ == '__main__':
+    @notify_function_call
+    def sum_(*args):
+        return sum(args)
+
+    
+    sum_(1,2,3,4,5,6,)
